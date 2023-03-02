dec057a76432cc2508095b80f120570920c0645b
Gdija <khadijaghadi00@gmail.com>
2023-03-02 08:33:16 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,8 +11,6 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-	i++;
-	}
 	if (s1[i] > s2[i])
 	{
 	return (s1[i] - s2[i]);
@@ -25,5 +23,8 @@ int _strcmp(char *s1, char *s2)
 	{
 	return (0);
 	}
+	i++;
+	}
+	return (0);
 }
 
