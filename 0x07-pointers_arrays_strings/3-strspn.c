6d2299521f762609066f27da1bf3c61eae4ca159
Gdija <khadijaghadi00@gmail.com>
2023-03-06 12:43:41 -0800

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *  _strspn - gets the length of a prefix substring
+ * @s: character
+ * @accept: character
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int len = 0;
+	int i = 0;
+	int j = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+	for (; accept[j] != '\0'; j++)
+	{
+	if (s[i] == accept[j])
+	{
+	len++;
+	break;
+	}
+	else if (accept[j + 1] == '\0')
+	{
+	return (len);
+	}
+	}
+	}
+	return (len);
+}
