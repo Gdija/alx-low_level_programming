e1e11d74fa36e92a9543dc534670e419e028ab2e
Gdija <khadijaghadi00@gmail.com>
2023-03-14 06:56:23 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_strdup - copy of the string given as a parameter
+ * @str: string
+ * Return: nothing
+ */
+char *_strdup(char *str)
+{
+	char *t;
+	int i = 0;
+	int len = 1;
+
+	while (str[len])
+	{
+	len++;
+	}
+	t = malloc((sizeof(char) * len) + 1);
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	if (t == 0)
+	{
+	return (NULL);
+	}
+	while (i < len)
+	{
+	t[i] = str[i];
+	i++;
+	}
+	return (t);
+}
