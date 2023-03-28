7668895667471977323c6251d43c31af1a861dca
Gdija <khadijaghadi00@gmail.com>
2023-03-28 10:53:12 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Define a new type
+ * @name: character
+ * @owner: character
+ * @age: float
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
