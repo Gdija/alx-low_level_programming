c57da375a334dfcdfb1239ffbc1b0c3005d04967
Gdija <khadijaghadi00@gmail.com>
2023-07-11 15:26:21 -0700

    2-append_text_to_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,15 +13,16 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | 0600);
-	if (fd == -1)
-		return (-1);
+
 	if (text_content == NULL)
 	{
 	text_content = "";
 	}
 	for (length = 0; text_content[length]; length++)
 		;
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+        if (fd == -1)
+                return (-1);
 	wr = write(fd, text_content, length);
 	if (wr == -1)
 		return (-1);
