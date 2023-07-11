ca6b1d7c2e33e97d96aa3875eb4da48cd84af54b
Gdija <khadijaghadi00@gmail.com>
2023-07-11 15:34:42 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,8 +21,8 @@ int create_file(const char *filename, char *text_content)
 	for (length = 0; text_content[length]; length++)
 		;
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-        if (fd == -1)
-                return (-1);
+	if (fd == -1)
+		return (-1);
 	wr = write(fd, text_content, length);
 	if (wr == -1)
 		return (-1);
