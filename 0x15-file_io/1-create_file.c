fac97319e164af229ba524a23c438fe92a48828f
Gdija <khadijaghadi00@gmail.com>
2023-07-10 18:32:15 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename:name of the file
+ * @text_content: text in the file
+ * Return: 1 on succes -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int length, wr;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+	text_content = "";
+	}
+	for (length = 0; text_content[length]; length++)
+		;
+	wr = write(fd, text_content, length);
+	if (wr == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
